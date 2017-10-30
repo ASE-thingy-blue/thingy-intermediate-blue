b2684cb0d44af24103008f7ee04906db9d01601b
Salim Hermidas <salim@hermidas.ch>
2017-10-30 18:39:37 +0000

    TER-17: Logging Node Output to File

--- startup.sh
+++ startup.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+LOG="/var/data/intermediate.log"
+echo "" > ${log}
+
 cd /usr/src/app
 
 # Print Debug Information
 /usr/bin/lsusb
 hciconfig
 
+(
+echo ""
 echo "executing startup-script..."
+echo "tuuid: ${tuuid}"
+echo "tapi: ${tapi}"
+echo "tpi: ${tpi}"
+echo "tuser: ${tuser}"
+echo "tcb: ${tcb}"
+echo ""
 
 # detect thingy
 if [ "$taction" == "detect" ]; then
@@ -19,3 +30,4 @@ if [ "$taction" == "run" ]; then
     echo "run: node . connect"
     node . connect ${tuuid} --api ${tapi} --pi ${tpi} --user ${tuser} --cb ${tcb}
 fi
+) |tee ${LOG}
