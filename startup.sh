7df33d8eb47fb040bd2abde15c533a3c7f8af8d0
Salim Hermidas <salim@hermidas.ch>
2017-12-17 09:56:38 +0100

    TER-55: logfile name with current date, use tee -a to not loose log info if application restarts.

--- startup.sh
+++ startup.sh
@@ -2,23 +2,29 @@
 
 # This script is used inside the Docker container to set up the Thingy Intermediate Gateway
 
-LOG="/var/data/intermediate.log"
-echo "" > ${LOG}
+DATE=`date +"%Y-%m-%d"`
+TIMESTAMP=`date +"%Y-%m-%d_%H-%M"`
+
+LOG="/var/data/intermediate.${DATE}.log"
+
+echo "************************************************************************************************************************" >> ${LOG}
+echo "*** Starting Thingy intermediate Gateway *** ${TIMESTAMP} **********************************************************" >> ${LOG}
+echo "************************************************************************************************************************" >> ${LOG}
 
 cd /usr/src/app
 
-# Print Debug Information
+(
+echo "USB and Bluetooth state for debug:"
 /usr/bin/lsusb
 hciconfig
-
-(
-echo ""
-echo "Executing startup-script..."
+echo "************************************************************************************************************************"
+echo "Executing startup-script with params:"
 echo "tuuid: ${tuuid}"
 echo "tapi: ${tapi}"
 echo "tpi: ${tpi}"
 echo "tuser: ${tuser}"
 echo "tcb: ${tcb}"
+echo "************************************************************************************************************************"
 echo ""
 
 # Detect Thingy
@@ -32,4 +38,4 @@ if [ "$taction" == "run" ]; then
     echo "Run: node . connect"
     node . connect ${tuuid} --api ${tapi} --pi ${tpi} --user "${tuser}" --cb ${tcb}
 fi
-) 2>&1 | tee ${LOG}
+) 2>&1 | tee -a ${LOG}
